d7fa046c17910db8f1914a36c2d3f90dd48dc91d
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2025-03-20 03:47:35 +0700

    feat: initial commit

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorConstants {
+  const ColorConstants._();
+  static Color primaryColor = const Color(0xFF2B2B2B);
+  static Color secondaryColor = const Color(0xFF8B8B8B);
+  static Color dangerColor = const Color(0xFFE7000B);
+}
---
+++ lib/constants/environment.dart
@@ -0,0 +1,5 @@
+class Env {
+  const Env._();
+
+  static const String baseUrl = "http://localhost:8000";
+}
